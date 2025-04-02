4608c3f1630035d79730938fbfcfa92cfafcf6c8
Tomlee Abila <tomleeabila@gmail.com>
2025-04-02 23:10:15 +0300

    feat: tuples_refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -2,18 +2,18 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-pub struct Student (u32, String, String)
+pub struct Student (u32, String, String);
 
 pub fn id(student: &Student) -> u32 {
     student.0
 }
 
 pub fn first_name(student: &Student) -> &str {
-    student.1
+    &student.1
 }
 
 pub fn last_name(student: &Student) -> &str {
-    student.2
+    &student.2
 }
 
 #[cfg(test)]
