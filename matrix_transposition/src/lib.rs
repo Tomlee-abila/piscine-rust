859ace0c9ba03784607b2fc32a23c421ff1cbe73
Tomlee Abila <tomleeabila@gmail.com>
2025-04-02 22:52:39 +0300

    feat: matrix_transposition

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,12 +1,11 @@
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
-
 #[derive(Debug, PartialEq, Eq)]
-pub struct Matrix((i32, i32), (i32, i32));
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-    Matrix((m.0.0, m.1.0), (m.0.1, m.1.1))
+   Matrix((m.0.0, m.1.0), (m.0.1, m.1.1))
 }
 
 #[cfg(test)]
@@ -17,12 +16,5 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn test_transpose() {
-        let matrix = Matrix((1, 2), (3, 4));
-        let expected = Matrix((1, 3), (2, 4));
-        assert_eq!(transpose(matrix), expected);
     }
 }
