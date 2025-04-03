564e632ab156023d51f719923766cd87f4b0daa2
Tomlee Abila <tomleeabila@gmail.com>
2025-04-03 12:49:42 +0300

    feat: initials

---
+++ initials/src/lib.rs
@@ -0,0 +1,32 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+
+    for name in names{
+        let mut initial_s: Vec<String> = Vec::new();
+
+        let names_ = name.trim().split(" ");
+
+        for n in names_{
+            if let Some(initial) = n.chars().next(){
+                initial_s.push(initial.to_string());
+            }
+        }
+        result.push(initial_s.join(" ."));
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
