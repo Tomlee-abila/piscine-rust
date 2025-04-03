57a218b76aa2cdd2048834e53a2e4c0b1c8c4b6e
Tomlee Abila <tomleeabila@gmail.com>
2025-04-04 01:52:24 +0300

    feat: copy

---
+++ borrow_me_the_reference/src/lib.rs
@@ -0,0 +1,71 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn delete_and_backspace(s: &mut String) {
+    let mut chars: Vec<char> = s.chars().collect();
+    let mut i = chars.len() ;	
+	while i  > 0 {
+		i -= 1;
+        if chars[i] == '+' {
+			chars.remove(i); 
+            if i < chars.len() {
+                chars.remove(i);
+            }
+        }
+    }	
+    i = 0;
+    while i < chars.len() {
+        if chars[i] == '-' {
+            if i > 0 {
+                chars.remove(i - 1);
+                i -= 1;
+            }
+            chars.remove(i); 
+        }  else {
+            i += 1;
+        }
+    }
+	
+    *s = chars.into_iter().collect();
+}
+
+
+
+
+
+
+pub fn do_operations(v: &mut Vec<String>) {
+    for element in v.iter_mut() {
+        let operator_index = element.chars().position(|c| c == '+' || c == '-');
+        if let Some(i) = operator_index {
+            let operator = element.chars().nth(i);
+            let (left, right) = element.split_at(i);
+            let x = left.trim().parse::<i32>().expect("invalid number");
+            let y = right.trim().parse::<i32>().expect("invalid number");
+            if let Some(o) = operator {
+                match o {
+                    '+' => {
+						*element = (x + y).to_string()
+					},
+                    '-' => {
+                        let y = -y;
+						*element = (x - y).to_string()
+					}
+					_ => println!("{} is not a valid operator!!",o)
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
