cc8231a9a4fbc8b779ece5ac5a2a11de21039fef
Tomlee Abila <tomleeabila@gmail.com>
2025-04-05 23:03:05 +0300

    feat: simple_hash

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -5,16 +5,17 @@ pub fn add(left: u64, right: u64) -> u64 {
 use std::collections::HashMap;
 
 pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
-	let mut frequency_count: HashMap<&str, usize> = HashMap::new();
-
-	for word in words{
-		*frequency_count.entry(word).or_insert(0) += 1;
-	}
-	frequency_count
+    let mut frequency_map = HashMap::new();
+    
+    for word in words {
+        *frequency_map.entry(word).or_insert(0) += 1;
+    }
+    
+    frequency_map
 }
 
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
-	frequency_count.len()
+    frequency_count.len()
 }
 
 #[cfg(test)]
