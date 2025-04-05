8b1ba8fc3faf4d6f2bc1e5d10da90ad906fbeaf4
Tomlee Abila <tomleeabila@gmail.com>
2025-04-05 22:17:11 +0300

    feat: capitalizing

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -16,21 +16,24 @@ pub fn capitalize_first(input: &str) -> String {
 }
 
 pub fn title_case(input: &str) -> String {
-	let input_r = input.replace("\t", " ");
-	input_r
-		.split_whitespace()
-			.map(|word|{
-				let mut result: String = String::new();
-				for (i, ch) in word.chars().enumerate(){
-					if i == 0 && ch.is_ascii_lowercase(){						
-						result.push(ch.to_ascii_uppercase().into());
-					}else{
-						result.push(ch);
-					}
-				}
-				result
-			}).collect::<Vec<String>>().join(" ")
-	
+    let mut result = String::new();
+    let mut in_whitespace = false;
+
+    for (i, ch) in input.chars().enumerate() {
+        if ch.is_whitespace() {
+            result.push(ch);
+            in_whitespace = true;
+        } else {
+            if in_whitespace || i == 0 {
+                result.push(ch.to_ascii_uppercase());
+                in_whitespace = false;
+            } else {
+                result.push(ch.to_ascii_lowercase());
+            }
+        }
+    }
+
+    result
 }
 
 pub fn change_case(input: &str) -> String {
