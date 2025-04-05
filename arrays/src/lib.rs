7de4de89e8c97f112f99f72baf51eac2d4df7f6e
Tomlee Abila <tomleeabila@gmail.com>
2025-04-05 19:01:02 +0300

    feat: card_deck

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -2,7 +2,14 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn sum(a: [i32; 32]) -> i32 {
+	a.iter().sum()
+}
 
+pub fn thirtytwo_tens() -> [i32; 32] {
+	let arr: [i32; 32] = [10; 32];
+    arr
+}
 
 #[cfg(test)]
 mod tests {
