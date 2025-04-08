1bfe73d3ea898f675fea727189eac1c326f3207c
Tomlee Abila <tomleeabila@gmail.com>
2025-04-09 00:24:38 +0300

    feat: unwrap_or_expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -1,18 +1,32 @@
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
-    match (server, security_level) {
-        // Success cases
-        (Ok(url), Security::Unknown) => url.to_string(),
-        (Ok(url), Security::Message) => url.to_string(),
-        (Ok(url), Security::Warning) => url.to_string(),
-        (Ok(url), Security::NotFound) => url.to_string(),
-        (Ok(url), Security::UnexpectedUrl) => panic!("{}", url),
-        
-        // Error cases
-        (Err(_msg), Security::Unknown) => panic!(),
-        (Err(_msg), Security::Message) => panic!("ERROR: program stops"),
-        (Err(_msg), Security::Warning) => "WARNING: check the server".to_string(),
-        (Err(msg), Security::NotFound) => format!("Not found: {}", msg),
-        (Err(msg), Security::UnexpectedUrl) => msg.to_string(),
+    match security_level {
+        Security::Unknown =>{
+            server.expect("").to_owned()
+        },
+        Security::Message => {
+            match server {
+                Ok(s)=> return s.to_owned(),
+                Err(_) => panic!("ERROR: program stops")
+            }
+        },
+        Security::Warning => {
+            match server {
+                Ok(s)=> return s.to_owned(),
+                Err(_) => return "WARNING: check the server".to_owned()
+            }
+        },
+        Security::NotFound => {
+            match server {
+                Ok(s)=> return s.to_owned(),
+                Err(e) => return format!("Not found: {}", e)
+            }
+        },
+        Security::UnexpectedUrl => {
+            match server {
+                Ok(s)=> panic!("{}", s),
+                Err(e) => return format!("{}", e)
+            }
+        }
     }
 }
 
