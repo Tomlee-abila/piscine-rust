ae9844903cb4d2875247c956395d6650d956f9af
kevwasonga <kevinwasonga116@gmail.com>
2025-04-09 23:19:44 +0300

    feat: cipher

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -1,26 +1,31 @@
 #[derive(Debug, PartialEq)]
 pub struct CipherError {
     pub expected: String,
-    pub found: String,
 }
 
 pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
-    let mut cipher: CipherError = CipherError {
-        expected: ciphered,
-        found: original.chars()
-        .map(|c|{
-            if c.is_uppercase() {
-                ('A'+'Z') - c
-            } else if c.is_lowercase(){
-                ('a'+'z') - c
-            }else{
-                c
-            }
-        }).collect::String(),
-    };
-    if cipher.expected == cipher.found {
-        Ok(())
-    } else {
-        Err(cipher.expected)
+    let res = atbash(original);
+
+    match res == ciphered {
+        true => Ok(()),
+        false => Err(CipherError{
+            expected: res,
+        })
     }
+}
+
+pub fn atbash(s: &str) -> String {
+    let mut s1 = String::from("");
+    for c in s.chars() {
+        if c >= 'a' && c <= 'z' {
+            let n = (122 - c as u8) + 97;
+            s1.push(n as char)
+        } else if c >= 'A' && c <= 'Z' {
+            let n = (90 - c as u8) + 65;
+            s1.push(n as char)
+        } else {
+            s1.push(c)
+        }
+    }
+    s1
 }
