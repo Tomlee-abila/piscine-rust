d7bdfc94216122e37a5c557e8eef36e058e4ccca
kevwasonga <kevinwasonga116@gmail.com>
2025-04-09 23:03:38 +0300

    feat: cipher

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -5,6 +5,22 @@ pub struct CipherError {
 }
 
 pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
-    let mut cipher = Cipher::new(original, ciphered);
-    cipher.cipher()
+    let mut cipher: CipherError = CipherError {
+        expected: ciphered,
+        found: original.chars()
+        .map(|c|{
+            if c.is_uppercase() {
+                ('A'+'Z') - c
+            } else if c.is_lowercase(){
+                ('a'+'z') - c
+            }else{
+                c
+            }
+        }).collect::String(),
+    };
+    if cipher.expected == cipher.found {
+        Ok(())
+    } else {
+        Err(cipher.expected)
+    }
 }
