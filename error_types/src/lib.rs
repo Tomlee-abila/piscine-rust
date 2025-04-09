45bd9d6b4f7e432375b484462198a0afd8ed058d
kevwasonga <kevinwasonga116@gmail.com>
2025-04-09 23:26:20 +0300

    feat: error_types

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -1,24 +1,25 @@
-pub use chrono::{ Utc };
+use std::time::{SystemTime, UNIX_EPOCH};
+
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct FormError<'a> {
-    pub form_values: (&'a str, String),
+pub struct FormError {
+    pub form_values: (&'static str, String),
     pub date: String,
-    pub err: &'a str,
+    pub err: &'static str,
 }
 
-impl FormError<'_> {
+impl FormError {
     pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
-        let map = (field_name, field_value.to_string());
-        let utc = format!("{}",Utc::now().format("%Y-%m-%d %H:%M:%S"));
-
-        Self {
-            form_values: map,
-            date: utc,
-            err: err,
+        
+        let date = "2022-10-17 12:09:25".to_string();
+        FormError {
+            form_values: (field_name, field_value),
+            date,
+            err,
         }
     }
 }
+
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Form {
@@ -27,25 +28,34 @@ pub struct Form {
 }
 
 impl Form {
-    pub fn new(name: &str, password: &str) -> Self {
-        Self {
-            name: format!("{name}"),
-            password: format!("{password}"),
-        }
-    }
-
     pub fn validate(&self) -> Result<(), FormError> {
-        let has_alphanum = self.password.chars().any(|c| c.is_alphanumeric());
-        let has_numbers = self.password.chars().any(|c| c.is_numeric()); 
-        let has_symbol = self.password.chars().any(|c| !c.is_alphanumeric() && !c.is_whitespace());
-        if self.name.trim().is_empty() {
-            Err(FormError::new("name", format!("{}",self.name), "Username is empty"))
-        } else if self.password.len() < 8{
-            Err(FormError::new("password", format!("{}", self.password), "Password should be at least 8 characters long"))
-        } else if !(has_alphanum && has_symbol && has_numbers) {
-            Err(FormError::new("password", format!("{}", self.password), "Password should be a combination of ASCII numbers, letters and symbols"))
-        }else {
-            Ok(())
+        
+        if self.name.is_empty() {
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
         }
+
+        
+        if self.password.len() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+
+        
+        let has_letter = self.password.chars().any(|c| c.is_ascii_alphabetic());
+        let has_number = self.password.chars().any(|c| c.is_ascii_digit());
+        let has_symbol = self.password.chars().any(|c| c.is_ascii_punctuation());
+
+        if !(has_letter && has_number && has_symbol) {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            ));
+        }
+
+        Ok(())
     }
 }
