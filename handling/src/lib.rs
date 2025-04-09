ba58c63feba459094ef01a04e3a61d97d28fa4d0
kevwasonga <kevinwasonga116@gmail.com>
2025-04-09 20:52:16 +0300

    feat: handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -10,5 +10,5 @@ pub fn open_or_create(file: &Path, content: &str) {
         .open(file)
         .expect("Failed to open or create file");
 
-    write!(file, "{}", content).unwrap();
+    write!(file, "{}", content).expect("Failed to write to file");
 }
