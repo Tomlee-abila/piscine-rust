b634b959a91b4fc6e83c4e366075dcbe3b538f0f
kevwasonga <kevinwasonga116@gmail.com>
2025-04-09 21:49:27 +0300

    feat: questio_mark

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -13,6 +13,6 @@ pub struct Four {
 
 impl One {
     pub fn get_fourth_layer(self) -> Option<u16> {
-        self.first_layer?.second_layer?.third_laye?.fourth_layer
+        self.first_layer?.second_layer?.third_layer?.fourth_layer
     }
 }
