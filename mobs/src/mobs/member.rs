afcbe689fe362c6ffb82e6e13cc091d92f8c237f
Tomlee Abila <tomleeabila@gmail.com>
2025-04-11 01:20:47 +0300

    mobs

--- mobs/src/mobs/member.rs
+++ mobs/src/mobs/member.rs
@@ -1,15 +1,33 @@
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Boss {
-    pub name: String,
-    pub age: u8,
+pub enum Role {
+    Underboss,
+    Caporegime,
+    Soldier,
+    Associate,
 }
+    
+        #[derive(Debug, Clone, PartialEq)]
+    pub struct Member {
+        pub name: String,
+        pub role: Role,
+        pub age: u8,
+    }
 
-impl Boss {
-    pub fn new(name: &str, age: u8) -> Boss {
-        Boss {
-            name: name.to_string(),
-            age: age,
+    impl Member {
+        pub fn get_promotion(&mut self) {
+            match self.role {
+                Role::Associate => self.role = Role::Soldier,
+                Role::Soldier => self.role = Role::Caporegime,
+                Role::Caporegime => self.role = Role::Underboss,
+                Role::Underboss => self.role = Role::Underboss,
+            }
+        }
+        pub fn new(name: &str, role: Role, age: u8) -> Member {
+            Member {
+                name: name.to_string(),
+                role: role,
+                age: age,
+            }
         }
     }
-}
