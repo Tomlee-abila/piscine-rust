0c6feda636d21f6572308cb567d6c0763ac6606f
Tomlee Abila <tomleeabila@gmail.com>
2025-04-12 09:32:07 +0300

    pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,13 +1,10 @@
 pub fn is_pangram(s: &str) -> bool {
-    let mut result: Vec<char> = Vec::new();
+    let mut letters = std::collections::HashMap::new();    
 
     for ch in s.chars(){
-        if !result.contains(&ch.to_ascii_lowercase()) && ch.is_ascii_alphabetic(){
-            result.push(ch.to_ascii_lowercase());
+        if ch.is_ascii_alphabetic(){
+            letters.insert(ch.to_ascii_lowercase(), true);
         }
     }
-    if result.len() == 26{
-        return  true;
-    }
-    false
+    letters.len() == 26
 }
