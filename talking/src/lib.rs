32c46f9da9c1cb054eec5bdefd463db53f9264d9
Tomlee Abila <tomleeabila@gmail.com>
2025-04-12 10:20:15 +0300

    feat: talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,38 +1,38 @@
-pub fn talking(text: &str) -> &str {
-    let text = text.trim();
-    let len = text.chars().count();
-    if text.is_empty(){
-        return "Just say something!";
-    }
+// pub fn talking(text: &str) -> &str {
+//     let text = text.trim();
+//     let len = text.chars().count();
+//     if text.is_empty(){
+//         return "Just say something!";
+//     }
 
-    let mut answers = std::collections::HashMap::new();
+//     let mut answers = std::collections::HashMap::new();
 
-    answers.insert("yell", true);      
-    answers.insert("question_yell", false);
-    answers.insert("question", false); 
+//     answers.insert("yell", true);      
+//     answers.insert("question_yell", false);
+//     answers.insert("question", false); 
 
-    for (i,ch) in text.chars().enumerate(){
-        if i == len-1 && ch == '?'{
-            if answers["yell"]{
-                answers.insert("question_yell", true);
-            }else {
-                answers.insert("question", true);
-            }
-            continue;
-        }
+//     for (i,ch) in text.chars().enumerate(){
+//         if i == len-1 && ch == '?'{
+//             if answers["yell"]{
+//                 answers.insert("question_yell", true);
+//             }else {
+//                 answers.insert("question", true);
+//             }
+//             continue;
+//         }
 
-        if ch.is_ascii_lowercase(){
-            answers.insert("yell", false);
-        }
-    }
+//         if ch.is_ascii_lowercase(){
+//             answers.insert("yell", false);
+//         }
+//     }
 
-    if answers["question_yell"]{
-        return "Quiet, I am thinking!";
-    }else if answers["question"]{
-         return "Sure.";
-    }else if answers["yell"] {
-        return "There is no need to yell, calm down!";
-    }else{
-        return "Interesting";
-    }
-}
+//     if answers["question_yell"]{
+//         return "Quiet, I am thinking!";
+//     }else if answers["question"]{
+//          return "Sure.";
+//     }else if answers["yell"] {
+//         return "There is no need to yell, calm down!";
+//     }else{
+//         return "Interesting";
+//     }
+// }
