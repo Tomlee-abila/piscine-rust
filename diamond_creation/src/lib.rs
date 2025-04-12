06da39a204f56e9c36e547d3b455e4e27a6c6c2b
Tomlee Abila <tomleeabila@gmail.com>
2025-04-13 00:31:49 +0300

    feat: diamond_creation

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -4,14 +4,19 @@ pub fn get_diamond(c: char) -> Vec<String> {
 
     for (i, ch) in ('A'..=c.to_ascii_uppercase()).enumerate(){
         let mut row: String;
+        let size = (len*2)-1;
+        let r = len-1-i;
         
-        row = format!("{}{}", " ".repeat(len-1-i), ch);
+        row = format!("{}{}", " ".repeat(r), ch);
         if i > 0{
-            row = format!("{}{}{}", row," ".repeat(i), ch);
+            row = format!("{}{}{}", row," ".repeat(size - (r*2)-2), ch);
         }
-        row = format!("{}{}", row," ".repeat(len-1-i));
+        row = format!("{}{}", row," ".repeat(r));
+
+        println!("i:{} len:{} row:[{}]",i, row.len(), row);
         
         result.push(row);
+        
     }
 
     for i in (0..len-1).rev(){
