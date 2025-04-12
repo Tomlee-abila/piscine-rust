a4f1e0762d776fdb9c364b3549557860cbd04a9c
Tomlee Abila <tomleeabila@gmail.com>
2025-04-12 12:42:43 +0300

    feat: logic_number

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,12 +1,11 @@
-pub fn number_logic(num: u32) -> bool {
-    let mut sum: u32 = 0;
+pub fn number_logic(num: u32) -> bool {    
+    let mut digits = Vec::new();
+    let mut copy = num;
 
-    let number = num.to_string();
-   
-    for (i, ch) in number.chars().enumerate(){        
-        sum += (ch as u32 - '0' as u32).pow(i as u32+1);
-        println!("i:{}, sum:{}, num:{}, ch:{}",i,sum, num, ch);
+    while copy > 0{
+        digits.push(copy%10);
+        copy /= 10;
     }
-    
-    num == sum
+    let len: u32 = digits.len() as u32;
+    digits.iter().map(| n| n.pow(len)).sum::<u32>() == num
 }
