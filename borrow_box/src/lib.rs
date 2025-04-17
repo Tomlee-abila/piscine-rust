67345414e50e8648f7a9282b159ec29426c89097
Tomlee Abila <tomleeabila@gmail.com>
2025-04-17 16:33:51 +0300

    feat: borrow_box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,51 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u16),
+    pub p2: (String, u16),
+    pub nb_games: u16
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u16) -> Box<GameSession> {
+        Box::new(GameSession{
+            id,
+            p1: (p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games
+        })
+    }
+
+    pub fn read_winner(&self) -> (String, u16) {
+        if self.p1.1 > self.p2.1{
+            (self.p1.0.clone(), self.p1.1)
+        }else if self.p1.1 < self.p2.1{
+            (self.p2.0.clone(), self.p2.1)
+        }else{
+            ("Same score! tied".to_string(), self.p2.1)
+        }
+    }
+    pub fn update_score(&mut self, user_name: String) {
+        if self.is_finished(){
+            return;
+        }
+        if self.p1.0.clone() == user_name{
+            self.p1.1 += 1;
+        }
+
+        if self.p2.0.clone() == user_name{
+            self.p2.1 += 1;
+        }
+
+    }
+    pub fn delete(self) -> String {
+        format!("game deleted: id -> {}", self.id)
+    }
+
+    pub fn is_finished(&self) -> bool{
+        if self.p1.1 + self.p2.1 == self.nb_games{
+            return true;
+        }
+        false
+    }
+}
