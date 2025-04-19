fc344858ee048ad228b2c87005be14722e70b331
Tomlee Abila <tomleeabila@gmail.com>
2025-04-19 12:44:49 +0300

    feat: easy_traits

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
+}
+
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(&mut self, str_to_append: String) -> Self {
+        self.value = format!("{}{}", self.value, str_to_append);
+        Self {
+            value: self.value.clone(),
+        }
+    }
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
+        self.value = format!("{}{}", self.value, nb_to_append);
+        Self {
+            value: self.value.clone(),
+        }
+    }
+
+    fn remove_punctuation_marks(&mut self) -> Self {
+        self.value = self.value.chars().filter(|panctuation| !panctuation.is_ascii_punctuation()).collect::<String>();
+        Self {
+            value: self.value.clone(),
+        }
+    }
+}
