619315965b39493af33df0a06a51a48b4c379eb3
Tomlee Abila <tomleeabila@gmail.com>
2025-04-19 12:53:52 +0300

    feat: easy_traits

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -5,30 +5,32 @@ pub struct StringValue {
 
 pub trait AppendStr {
     fn append_str(&mut self, str_to_append: String) -> Self;
-
     fn append_number(&mut self, nb_to_append: f64) -> Self;
-
     fn remove_punctuation_marks(&mut self) -> Self;
 }
 
 impl AppendStr for StringValue {
     fn append_str(&mut self, str_to_append: String) -> Self {
-        self.value = format!("{}{}", self.value, str_to_append);
-        Self {
+        self.value.push_str(&str_to_append);
+        StringValue {
             value: self.value.clone(),
         }
     }
 
     fn append_number(&mut self, nb_to_append: f64) -> Self {
-        self.value = format!("{}{}", self.value, nb_to_append);
-        Self {
+        self.value.push_str(&nb_to_append.to_string());
+        StringValue {
             value: self.value.clone(),
         }
     }
 
     fn remove_punctuation_marks(&mut self) -> Self {
-        self.value = self.value.chars().filter(|panctuation| !panctuation.is_ascii_punctuation()).collect::<String>();
-        Self {
+        self.value = self
+            .value
+            .chars()
+            .filter(|&c| c != '.' && c != ',' && c != '?' && c != '!')
+            .collect();
+        StringValue {
             value: self.value.clone(),
         }
     }
