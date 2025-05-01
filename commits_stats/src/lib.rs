5a5b451c5d404a41c5ffe2c9784de9bdccf4fdc9
Tomlee Abila <tomleeabila@gmail.com>
2025-05-01 17:50:38 +0300

    update

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -1,44 +1,34 @@
-use std::{collections::HashMap, fs};
+use std::collections::HashMap;
+use chrono::{DateTime, Datelike, Utc};
 use json::JsonValue;
-use chrono::{DateTime, Utc, Datelike};
 
-pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
-    let mut week_counts = HashMap::new();
-    
-    if !data.is_array() {
-        return week_counts;
-    }
+/// Returns number of commits per author (GitHub login).
+pub fn commits_per_author(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
 
     for commit in data.members() {
-        let date_str = commit["commit"]["author"]["date"]
-            .as_str()
-            .unwrap_or("");
-        
-        if let Ok(date) = DateTime::parse_from_rfc3339(date_str) {
-            let utc_date: DateTime<Utc> = date.with_timezone(&Utc);
-            let year = utc_date.year();
-            let week = utc_date.iso_week().week();
-            let week_key = format!("{}-W{}", year, week);
-            
-            *week_counts.entry(week_key).or_insert(0) += 1;
+        if let Some(author_login) = commit["author"]["login"].as_str() {
+            *map.entry(author_login.to_string()).or_insert(0) += 1;
         }
     }
-    
-    week_counts
+
+    map
 }
 
-pub fn commits_per_author(data: &JsonValue) -> HashMap<String, u32> {
-    let mut author_counts = HashMap::new();
-    
-    if !data.is_array() {
-        return author_counts;
-    }
+/// Returns number of commits per week (format: "YYYY-W##").
+pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
 
     for commit in data.members() {
-        if let Some(login) = commit["author"]["login"].as_str() {
-            *author_counts.entry(login.to_string()).or_insert(0) += 1;
+        if let Some(date_str) = commit["commit"]["author"]["date"].as_str() {
+            if let Ok(datetime) = DateTime::parse_from_rfc3339(date_str) {
+                let datetime_utc = datetime.with_timezone(&Utc);
+                let iso_week = datetime_utc.iso_week();
+                let key = format!("{}-W{}", iso_week.year(), iso_week.week());
+                *map.entry(key).or_insert(0) += 1;
+            }
         }
     }
-    
-    author_counts
-}   
+
+    map
+}
